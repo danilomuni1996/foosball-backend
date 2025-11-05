e681fb40d4577feaf7fbca3eb0d09561cf2eaac4
danilomuni1996 <danilomuni1996@gmail.com>
2025-11-05 14:05:34 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+
+class Player(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    photo_url: Optional[str] = None
+    preferred_role: Optional[str] = None  # "attaccante" | "portiere"
+    points: int = 0
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+class Match(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    date: datetime = Field(default_factory=datetime.utcnow)
+    teamA_attacker_id: int
+    teamA_goalkeeper_id: int
+    teamB_attacker_id: int
+    teamB_goalkeeper_id: int
+    score_a: int
+    score_b: int
+    winner_team: str  # "A" | "B"
+    points_awarded: str  # JSON testuale
