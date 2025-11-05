39fed91d53b958ab4962393ca2c178277566fd8a
danilomuni1996 <danilomuni1996@gmail.com>
2025-11-05 20:30:43 +0100

    Update models.py

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class Player(SQLModel, table=True):
 
 class Match(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    date: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
     teamA_attacker_id: int
     teamA_goalkeeper_id: int
     teamB_attacker_id: int
